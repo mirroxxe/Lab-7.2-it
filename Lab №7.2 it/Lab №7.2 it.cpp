1741325ddd8f8d36ed1b011a1378e82482bba73f
Volodya <chuchman002@gmail.com>
2023-11-10 00:59:46 +0200

    Add project files.

---
+++ Lab №7.2 it/Lab №7.2 it.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <ctime>
+
+void generateMatrix(int matrix[][5], int n) {
+    srand(time(0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = rand() % 100;
+        }
+    }
+}
+
+void printMatrix(int matrix[][5], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << matrix[i][j] << "\t";
+        }
+        std::cout << "\n";
+    }
+}
+
+int findMaxInMainDiagonal(int matrix[][5], int n) {
+    int maxElem = matrix[0][0];
+    for (int i = 1; i < n; i++) {
+        if (matrix[i][i] > maxElem) {
+            maxElem = matrix[i][i];
+        }
+    }
+    return maxElem;
+}
+
+int main() {
+    int n = 5;
+    int matrix[5][5];
+    generateMatrix(matrix, n);
+
+    std::cout << "matrix:\n";
+    printMatrix(matrix, n);
+
+    int maxElem = findMaxInMainDiagonal(matrix, n);
+    std::cout << "max element diagonally: " << maxElem << "\n";
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab №7.2 it/Lab №7.2 it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1  
+{
+	TEST_CLASS(UnitTest1)  
+	{
+	public:
+
+		TEST_METHOD(FindMaxMainDiagonal)
+		{
+			const int n = 5;
+			int matrix[n][n] = {
+				{96, 96, 80, 57, 68},
+				{69, 78, 9, 20, 25},
+				{40, 60, 55, 49, 23},
+				{8, 58, 50, 56, 39},
+				{39, 20, 43, 39, 34}
+			};
+
+			int result = findMaxInMainDiagonal(matrix, n);
+
+			Assert::AreEqual(96, result);  
+		}
+	};
+}
